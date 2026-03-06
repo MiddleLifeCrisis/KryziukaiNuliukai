7c1d64b310dff5fb92d217baed7944c118d10851
Justas Osipovas <justas.osipovas@gmail.com>
2026-03-06 15:56:11 +0200

    v2  pridetas kad keiciasi zaidejai,  strigau, kad jei zaidejas iveda klaidingai ant esancio simbolio, ismeta klaida, bet pakartoti jam paciam neduoda.  Acion Item: debug ta vietą.

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#parodyti TIC TAC TOE Logo
 from urllib.parse import splitquery
 
 from pyfiglet import figlet_format
@@ -12,56 +11,42 @@ grid = {
     (0, 0): " ", (1, 0): " ", (2, 0): " ",  # APATINĖ EILUTĖ
 }
 
-print(f"{grid[(0,0)]} | {grid[(1,0)]} | {grid[(2,0)]}  2")
+print(f"{grid[(0, 2)]} | {grid[(1, 2)]} | {grid[(2, 2)]} 2")
 print("---------")
-print(f"{grid[(0,1)]} | {grid[(1,1)]} | {grid[(2,1)]}  1")
+print(f"{grid[(0, 1)]} | {grid[(1, 1)]} | {grid[(2, 1)]} 1")
 print("---------")
-print(f"{grid[(0,2)]} | {grid[(1,2)]} | {grid[(2,2)]}  0")
+print(f"{grid[(0, 0)]} | {grid[(1, 0)]} | {grid[(2, 0)]} 0")
 print("")
 print(" 0  1  2 ")
+print("")
 
+zaidejas = "X"
 
-# isiminti simbolio kordinate
+while True:
 
+    ejimasX_x = input(f"žaidėjas {zaidejas} įveskite kordinates x (0-2): ")
+    ejimasX_y = input(f"žaidėjas {zaidejas} Įveskite kordinates y (0-2): ")
+    x_skaicius = int(ejimasX_x)
+    y_skaicius = int(ejimasX_y)
 
-# prasyti user 2 ivesti kordinates
-# isiminti simbolio kordinate
-# parodyti gride priskirta simoboli ir pries tai buvusius simbolius
+    koordinate = (x_skaicius, y_skaicius)
 
-# patikrinti ar neatsirado laimetojas
+    if grid[koordinate] == " ":
+        grid[koordinate] = zaidejas
+    else:
+        print("Langelis užimtas, parašykite kordinates iš naujo")
 
-grid = {
-    (0, 2): " ", (1, 2): " ", (2, 2): " ",  # VIRŠUTINĖ EILUTĖ
-    (0, 1): " ", (1, 1): " ", (2, 1): " ",  # VIDURINĖ EILUTĖ
-    (0, 0): " ", (1, 0): " ", (2, 0): " ",  # APATINĖ EILUTĖ
-}
+    print(f"{grid[(0,2)]} | {grid[(1,2)]} | {grid[(2,2)]} 2")
+    print("---------")
+    print(f"{grid[(0,1)]} | {grid[(1,1)]} | {grid[(2,1)]} 1")
+    print("---------")
+    print(f"{grid[(0,0)]} | {grid[(1,0)]} | {grid[(2,0)]} 0")
+    print("")
+    print(" 0  1  2 ")
+    print("")
 
-ejimasX_x = input("Įveskite kordinates x (0-2): ")
-ejimasX_y = input("Įveskite kordinates y (0-2): ")
-x_skaicius = int(ejimasX_x)
-y_skaicius = int(ejimasX_y)
+    if zaidejas == "X":
+        zaidejas = "O"
+    else:
+        zaidejas = "X"
 
-koordinate = (x_skaicius, y_skaicius)
-
-grid[koordinate] = "X"
-
-print(f"{grid[(0,2)]} | {grid[(1,2)]} | {grid[(2,2)]}")
-print("---------")
-print(f"{grid[(0,1)]} | {grid[(1,1)]} | {grid[(2,1)]}")
-print("---------")
-print(f"{grid[(0,0)]} | {grid[(1,0)]} | {grid[(2,0)]}")
-
-
-
-
-
-# if grid["0,"] == thisdict["b1"]:
-#     print("True")
-# else:
-#     print("False")
-
-# kartoti kol neatsiranda laimetojas
-
-# paskelbti nugaletoja
-# paskbelti nugaletoju list pagal best taskus ever
-# prideti mano reklama
