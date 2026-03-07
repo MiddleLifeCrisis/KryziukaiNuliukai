b548c87b5b066419b08468a72b7aceaed5119485
Justas Osipovas <justas.osipovas@gmail.com>
2026-03-07 17:38:22 +0200

    v3 tikrinimas eiluciu, stulpeliu, istrizainiu next - lygiuju aptikimas

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-from urllib.parse import splitquery
-
 from pyfiglet import figlet_format
-
 
 print(figlet_format("TIK TAC TOE"))
 
@@ -24,8 +21,8 @@ zaidejas = "X"
 
 while True:
 
-    ejimasX_x = input(f"žaidėjas {zaidejas} įveskite kordinates x (0-2): ")
-    ejimasX_y = input(f"žaidėjas {zaidejas} Įveskite kordinates y (0-2): ")
+    ejimasX_x = input(f"žaidėjas '{zaidejas}' įveskite simbolio x ašies koordinates 0/1/2: ")
+    ejimasX_y = input(f"žaidėjas {zaidejas} įveskite simbolio y ašies koordinates 0/1/2: ")
     x_skaicius = int(ejimasX_x)
     y_skaicius = int(ejimasX_y)
 
@@ -33,8 +30,12 @@ while True:
 
     if grid[koordinate] == " ":
         grid[koordinate] = zaidejas
+        if zaidejas == "X":
+            zaidejas = "O"
+        else:
+            zaidejas = "X"
     else:
-        print("Langelis užimtas, parašykite kordinates iš naujo")
+        print("Langelis užimtas, parašykite koordinates iš naujo!")
 
     print(f"{grid[(0,2)]} | {grid[(1,2)]} | {grid[(2,2)]} 2")
     print("---------")
@@ -45,8 +46,54 @@ while True:
     print(" 0  1  2 ")
     print("")
 
-    if zaidejas == "X":
-        zaidejas = "O"
+#virustines eilutes tikrinimas
+    if grid[(0,2)] == "X" and grid[(1,2)] == "X" and grid[(2,2)] == "X":
+        print("Laimėjo žaidėjas 'X'")
+        break
+    if grid[(0,2)] == "O" and grid[(1,2)] == "O" and grid[(2,2)] == "O":
+        print("Laimėjo žaidėjas 'O'")
+        break
+#vidurines eilutes tikrinimas
+    if grid[(0,1)] == "X" and grid[(1,1)] == "X" and grid[(2,1)] == "X":
+        print("Laimėjo žaidėjas 'X'")
+        break
+    if grid[(0,1)] == "O" and grid[(1,1)] == "O" and grid[(2,1)] == "O":
+        print("Laimėjo žaidėjas 'O'")
+        break
+#vapatines eilutes tikrinimas
+    if grid[(0,0)] == "X" and grid[(1,0)] == "X" and grid[(2,0)] == "X":
+        print("Laimėjo žaidėjas 'X'")
+        break
+    if grid[(0,1)] == "O" and grid[(1,0)] == "O" and grid[(2,0)] == "O":
+        print("Laimėjo žaidėjas 'O'")
+        break
+#pirmo stulpelio tikrinimas
+    if grid[(0,2)] == "X" and grid[(0,1)] == "X" and grid[(0,0)] == "X":
+        print("Laimėjo žaidėjas 'X'")
+        break
+    if grid[(0,2)] == "O" and grid[(1,2)] == "O" and grid[(2,2)] == "O":
+        print("Laimėjo žaidėjas 'O'")
+        break
+#antro stulpelio tikrinimas
+    if grid[(1,2)] == "X" and grid[(1,1)] == "X" and grid[(1,0)] == "X":
+        print("Laimėjo žaidėjas 'X'")
+        break
+    if grid[(1,2)] == "O" and grid[(1,1)] == "O" and grid[(1,0)] == "O":
+        print("Laimėjo žaidėjas 'O'")
+        break
+#trecio stulpelio tikrinimas
+    if grid[(2,2)] == "X" and grid[(2,1)] == "X" and grid[(2,0)] == "X":
+        print("Laimėjo žaidėjas 'X'")
+        break
+    if grid[(2,2)] == "O" and grid[(2,1)] == "O" and grid[(2,0)] == "O":
+        print("Laimėjo žaidėjas 'O'")
+        break
+#pirmos istrizaines tikrinimas
+    if grid[(0,2)] == "X" and grid[(1,1)] == "X" and grid[(2,0)] == "X":
+        print("Laimėjo žaidėjas 'X'")
+        break
+    if grid[(0,2)] == "O" and grid[(1,1)] == "O" and grid[(2,0)] == "O":
+        print("Laimėjo žaidėjas 'O'")
+        break
     else:
-        zaidejas = "X"
-
+        print("Lygiosios")
